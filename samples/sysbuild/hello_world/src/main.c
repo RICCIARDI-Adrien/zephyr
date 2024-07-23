1963185db7bf256006b1d7b7c3223de2c2f058af
Adrien Ricciardi <aricciardi@baylibre.com>
2024-07-23 11:33:56 +0200

    [WIP] Added a test code to the hello_world that displays a prepopulated NVS value.
    Also added the STM32F091RC Nucleo flash configuration device tree overlay to make real world tests.

--- samples/sysbuild/hello_world/src/main.c
+++ samples/sysbuild/hello_world/src/main.c
@@ -3,11 +3,46 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <string.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/fs/nvs.h>
+#include <zephyr/storage/flash_map.h>
 #include <zephyr/sys/printk.h>
+
+#define NVS_PARTITION storage_partition
 
 int main(void)
 {
+	int rc = 0;
+	struct nvs_fs fs;
+	struct flash_pages_info flash_info;
+	uint8_t buf[32];
+
 	printk("Hello world from %s\n", CONFIG_BOARD_TARGET);
+
+	fs.flash_device = FIXED_PARTITION_DEVICE(NVS_PARTITION);
+	fs.offset = FIXED_PARTITION_OFFSET(NVS_PARTITION);
+	rc = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &flash_info);
+	if (rc) {
+		printk("Unable to get page info\n");
+		return 0;
+	}
+	fs.sector_size = flash_info.size;
+	fs.sector_count = FIXED_PARTITION_SIZE(NVS_PARTITION) / fs.sector_size;
+
+	rc = nvs_mount(&fs);
+	if (rc) {
+		printk("Flash init failed\n");
+		return 0;
+	}
+
+	rc = nvs_read(&fs, 12, buf, sizeof(buf));
+	if (rc < 0)
+		printk("Failed to read ID 12 (%s)\n", strerror(-rc));
+	else {
+		buf[sizeof(buf) - 1] = 0;
+		printf("ID 12 : \"%s\".\n", buf);
+	}
 
 	return 0;
 }
--- samples/sysbuild/hello_world/sysbuild.cmake
+++ samples/sysbuild/hello_world/sysbuild.cmake
@@ -5,7 +5,8 @@ set(FACTORY_BASE_DIR ${ZEPHYR_BASE}/samples/subsys/nvs/factory)
 
 ExternalProject_Add(nvs_factory_tool
   SOURCE_DIR ${FACTORY_BASE_DIR}
-  CMAKE_ARGS -DFACTORY_FLASH_CONTROLLER_OVERLAY=${FACTORY_BASE_DIR}/demo_flash_controller.overlay
+  CMAKE_ARGS
+  -DFACTORY_FLASH_CONTROLLER_OVERLAY=${ZEPHYR_BASE}/samples/sysbuild/hello_world/nucleo_f091rc_flash.overlay
   -DFACTORY_NVS_CONTENT_FILE=${FACTORY_BASE_DIR}/src/demo_nvs_content.c
   -DFACTORY_NVS_PARTITION_DT_LABEL=storage_partition
   -DBOARD=native_sim
