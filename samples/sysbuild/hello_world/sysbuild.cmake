bdb83473e2663d99667fbab56542a482fb90c557
Adrien Ricciardi <aricciardi@baylibre.com>
2024-08-01 13:50:07 +0200

    [WIP] Moved the binary to C conversion to a CMake test into the factory NVS project, so the code is not duplicated into the calling project.

--- samples/sysbuild/hello_world/sysbuild.cmake
+++ samples/sysbuild/hello_world/sysbuild.cmake
@@ -2,25 +2,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set(FACTORY_BASE_DIR ${ZEPHYR_BASE}/samples/subsys/nvs/factory)
-set(INCLUDE_OUTPUT_DIR ${APPLICATION_BINARY_DIR}/include)
 
 ExternalProject_Add(nvs_factory_tool
   SOURCE_DIR ${FACTORY_BASE_DIR}
   CMAKE_ARGS
   -DFACTORY_BINARY_DIR=${APPLICATION_BINARY_DIR}/factory
   -DFACTORY_NVS_CONTENT_FILE=${FACTORY_BASE_DIR}/src/demo_nvs_content.c
+  -DFACTORY_NVS_OUTPUT_FILE=${APPLICATION_BINARY_DIR}/include/nvs_area.h
   -DFACTORY_NVS_PARTITION_DT_LABEL=storage_partition
+  -DFACTORY_NVS_LINKER_SECTION=nvs_partition
   -DBOARD=native_sim
   -DFACTORY_FLASH_ERASE_VALUE=0xFF
   -DFACTORY_FLASH_ERASE_BLOCK_SIZE=0x800
   -DFACTORY_FLASH_WRITE_BLOCK_SIZE=0x2
   -DFACTORY_NVS_PARTITION_SIZE=0x1800
   INSTALL_COMMAND ""
-  TEST_COMMAND zephyr/zephyr.exe --flash=${APPLICATION_BINARY_DIR}/nvs_area.bin &&
-  mkdir -p ${INCLUDE_OUTPUT_DIR} &&
-  xxd -i ${APPLICATION_BINARY_DIR}/nvs_area.bin > ${INCLUDE_OUTPUT_DIR}/nvs_area.h &&
-  sed -i -e "1d" ${INCLUDE_OUTPUT_DIR}/nvs_area.h &&
-  sed -i -e "1i __in_section(nvs_partition, static, var) unsigned char nvs_area[] = {" ${INCLUDE_OUTPUT_DIR}/nvs_area.h
   TEST_AFTER_INSTALL TRUE
 )
 add_dependencies(${DEFAULT_IMAGE} nvs_factory_tool)
