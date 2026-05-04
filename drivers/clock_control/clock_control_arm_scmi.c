c36457019191d82ed198d3a489d3c1abe71da0a0
Adrien Ricciardi <aricciardi@baylibre.com>
2026-05-04 15:42:21 +0200

    Suppr printk de debug clocks et SCMI

--- drivers/clock_control/clock_control_arm_scmi.c
+++ drivers/clock_control/clock_control_arm_scmi.c
@@ -43,7 +43,6 @@ static int scmi_clock_on_off(const struct device *dev,
 	proto = dev->data;
 	data = proto->data;
 	clk_id = POINTER_TO_UINT(clk);
-	printk("[%s:%s:%d] entry dev=%s, clkid=%u\n", __FILE__, __func__, __LINE__, dev->name, clk_id);
 
 	if (clk_id >= data->clk_num) {
 		return -EINVAL;
