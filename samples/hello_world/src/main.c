7f0d9f3312571d38bc0215b50275ccaff6f40f0d
Adrien Ricciardi <aricciardi@baylibre.com>
2026-05-07 14:14:25 +0200

    Réception de trame OK dans le handler d'interruption (avec uniquement canal 0)

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -10,14 +10,20 @@
 //static const struct device *can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
 
 static const struct device *can_dev = DEVICE_DT_GET(DT_NODELABEL(can0)); //DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
-static const struct device *can_dev_1 = DEVICE_DT_GET(DT_NODELABEL(can1));
+//static const struct device *can_dev_1 = DEVICE_DT_GET(DT_NODELABEL(can1));
+
+static void can_0_rx_callback(const struct device *dev, struct can_frame *frame, void *user_data)
+{
+	printk("Trame reçue ! ID=%X, DLC=%u.\n", frame->id, frame->dlc);
+}
 
 int main(void)
 {
 	int ret, cnt = 1;
 	struct can_frame frame;
+	struct can_filter filter;
 
-	printk("Hello World CAN bordel! %s\n", CONFIG_BOARD_TARGET);
+	printk("TEST CAN sur %s\n", CONFIG_BOARD_TARGET);
 
 	/*ret = can_set_mode(can_dev, CAN_MODE_NORMAL);
 	if (ret != 0)
@@ -33,16 +39,28 @@ int main(void)
 		return 0;
 	}
 
-	ret = can_start(can_dev_1);
-	if (ret != 0)
+	// Receive all frames
+	filter.id = 0;
+	filter.mask = 0;
+	filter.flags = 0;
+	ret = can_add_rx_filter(can_dev, can_0_rx_callback, NULL, &filter);
+	if (ret < 0)
 	{
-		printk("err can_start dev 1() %d.\n", ret);
+		printk("err can_add_rx_filter() %d.\n", ret);
 		return 0;
 	}
+	printk("CAN 0 RX filter ID : %d.\n", ret);
+
+	/*ret = can_start(can_dev_1);
+	if (ret != 0)
+	{
+		printk("err can_start() dev 1 %d.\n", ret);
+		return 0;
+	}*/
 
 	while (1)
 	{
-		printk("\033Envoi trame %d\033[0m\n", cnt);
+		printk("\033[32mEnvoi trame %d\033[0m\n", cnt);
 		cnt++;
 
 		frame.id = 0x3F0;
@@ -60,19 +78,20 @@ int main(void)
 		}
 
 		/*frame.id = 0x3F1;
-		frame.dlc = can_bytes_to_dlc(2);
+		frame.dlc = can_bytes_to_dlc(4);
 		frame.flags = 0;
-		frame.data[0] = 0xBA;
-		frame.data[1] = 0xBE;
+		frame.data[0] = cnt >> 24;
+		frame.data[1] = cnt >> 16;
+		frame.data[2] = cnt >> 8;
+		frame.data[3] = (unsigned char) cnt;
 		ret = can_send(can_dev_1, &frame, K_FOREVER, NULL, NULL);
 		if (ret != 0)
 		{
-			printk("err can_send() %d.\n", ret);
+			printk("err can_send() dev 1 %d.\n", ret);
 			return 0;
-		}
-		printk("\033[32mTrame envoyee\033[0m\n");*/
+		}*/
 
-		k_msleep(100);
+		k_msleep(3000);
 	}
 
 	return 0;
